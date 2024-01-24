ef0b23d88e4bf3bd4ed8774e474494d3c5682ccc
reeganpinto <reegan.pinto@gmail.com>
2024-01-24 14:24:33 +0530

    moneyminder

--- application-1-frontend/lib/main.dart
+++ application-1-frontend/lib/main.dart
@@ -3,14 +3,16 @@
 // ignore_for_file: unused_import
 
 // Import necessary packages and files
-import 'package:finku_clone_slicing/Pages/Home%20Page/create_invoice.dart';
-import 'package:finku_clone_slicing/Pages/Home%20Page/investment_page.dart';
-import 'package:finku_clone_slicing/Pages/Home%20Page/make_a_budget.dart';
+import 'package:MoneyMinder/Utils/routes.dart';
+
 import 'package:flutter/material.dart';
-import 'package:finku_clone_slicing/Pages/Home%20Page/home_page.dart';
-import 'package:finku_clone_slicing/Pages/Home%20Page/login_page.dart';
-import 'package:finku_clone_slicing/Utils/routes.dart';
-import 'make_a_budget.dart';
+
+import 'Pages/Home Page/create_invoice.dart';
+import 'Pages/Home Page/home_page.dart';
+import 'Pages/Home Page/investment_page.dart';
+import 'Pages/Home Page/login_page.dart';
+import 'Pages/Home Page/make_a_budget.dart';
+
 
 void main() {
   runApp(MyApp());
