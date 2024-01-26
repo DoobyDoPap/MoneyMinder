9c0b1f3537377a602d999032fd7ce48a8f79d629
Chandan Bisht <chandanbisht@Chandans-MacBook-Air.local>
2024-01-26 12:39:19 +0530

    MoneyMinder

---
+++ application-1-frontend/lib/Pages/Home Page/splash_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class SplashScreenPage extends StatelessWidget {
+  const SplashScreenPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(230, 5, 9, 79),
+      body: GestureDetector(
+        onTap: () {
+          Navigator.pushReplacementNamed(context, '/login');
+        },
+        child: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Money',
+                    style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                          fontSize: 60.0,
+                          color: Color.fromRGBO(255, 255, 255, 1),
+                          fontStyle: FontStyle.italic,
+                          letterSpacing: 1.5,
+                        ),
+                  ),
+                  Text(
+                    'with a single tap',
+                  ),
+                ],
+              ),
+              Image.asset(
+                'assets/images/logo.png',
+                height: 100.0,
+                width: 100.0,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- application-1-frontend/lib/main.dart
+++ application-1-frontend/lib/main.dart
@@ -6,6 +6,7 @@
 import 'package:finku_clone_slicing/Pages/Home%20Page/create_invoice.dart';
 import 'package:finku_clone_slicing/Pages/Home%20Page/investment_page.dart';
 import 'package:finku_clone_slicing/Pages/Home%20Page/make_a_budget.dart';
+import 'package:finku_clone_slicing/Pages/Home%20Page/splash_page.dart';
 import 'package:flutter/material.dart';
 import 'package:finku_clone_slicing/Pages/Home%20Page/home_page.dart';
 import 'package:finku_clone_slicing/Pages/Home%20Page/login_page.dart';
@@ -23,7 +24,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      initialRoute: '/login',
+      initialRoute: '/splash',
       routes: {
         "/": (context) => LoginPage(),
         MyRoutes.homeRoute: (context) => HomePage(),
@@ -31,6 +32,7 @@ class MyApp extends StatelessWidget {
         MyRoutes.budgetRoute: (context) => BudgetPage(),
         MyRoutes.invoiceRoute: (context) => InvoicePage(),
         MyRoutes.investmentRoute: (context) => InvestmentOptionsPage(),
+        MyRoutes.splashRoute:(context) => SplashScreenPage(),
       },
     );
   }
