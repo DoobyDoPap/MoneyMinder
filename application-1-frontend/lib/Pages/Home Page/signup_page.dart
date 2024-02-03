5da65d479a03605d7d06db4c34d3b3cbc22e6306
Veeransh14 <shahveeransh@gmail.com>
2024-02-03 15:02:01 +0530

    MoneyMinder

--- application-1-frontend/lib/Pages/Home Page/signup_page.dart
+++ application-1-frontend/lib/Pages/Home Page/signup_page.dart
@@ -1,211 +1,202 @@
-// ignore_for_file: avoid_web_libraries_in_flutter, unused_import, unused_local_variable
+// ignore_for_file: unused_local_variable
 
-import 'package:MoneyMinder/Pages/Home%20Page/home_page.dart';
-import 'package:MoneyMinder/Pages/Home%20Page/login_page.dart';
-import 'package:MoneyMinder/Pages/Home%20Page/uihelper.dart';
+import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class SignUpPage extends StatelessWidget {
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
-  Future<void> signUp(BuildContext context, String email, String password,
-      String username) async {
-    if (email.isEmpty || password.isEmpty || username.isEmpty) {
-      UiHelper.CustomAlertBox(context, "Enter all required fields");
-    } else {
-      try {
-        UserCredential userCredential = await FirebaseAuth.instance
-            .createUserWithEmailAndPassword(email: email, password: password);
+  void _signUp(BuildContext context) async {
+    try {
+      UserCredential userCredential =
+          await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: _emailController.text,
+        password: _passwordController.text,
+      );
 
-        // After successful registration, update user profile with username
-        await userCredential.user?.updateProfile(displayName: username);
+      // Save user details to Firestore
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc(userCredential.user!.uid)
+          .set({
+        'username': _usernameController.text,
+        'email': _emailController.text,
+      });
 
-        // You can also store additional user data in Firebase Firestore or Realtime Database
-        // For example, storing user's username in Firestore
-        // FirebaseFirestore.instance.collection('users').doc(userCredential.user?.uid).set({
-        //   'username': username,
-        // });
-
-        print("Account Created Successfully");
-
-        // Navigate to home page
-        Navigator.pushReplacementNamed(context, '/home');
-      } on FirebaseAuthException catch (ex) {
-        UiHelper.CustomAlertBox(context, ex.code.toString());
-        print(ex.toString());
+      // After successful signup, navigate to the home page
+      Navigator.pushReplacementNamed(context, '/home');
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        _showErrorDialog(context, 'The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        _showErrorDialog(context, 'The account already exists for that email.');
+      } else {
+        _showErrorDialog(context, 'Error: ${e.message}');
       }
+    } catch (e) {
+      _showErrorDialog(context, 'Failed to sign up. Please try again.');
     }
   }
 
+  void _navigateToLogin(BuildContext context) {
+    // Directly navigate to the login page
+    Navigator.pushReplacementNamed(context, '/login');
+  }
+
+  void _showErrorDialog(BuildContext context, String errorMessage) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Error'),
+          content: Text(errorMessage),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('OK', style: TextStyle(color: Colors.red)),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Sign Up'),
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [
-                Color(0xFF0088FF), // Light Blue
-                Color(0xFF004080), // Dark Blue
-              ],
-            ),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 32.0),
-                  child: Column(
-                    children: [
-                      Image.asset(
-                        'assets/images/logo.png',
-                        height: 150.0,
-                        width: 150.0,
-                        color: Colors.white,
-                      ),
-                      SizedBox(height: 16.0),
-                      Text(
-                        'MoneyMinder',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 24.0,
-                          fontWeight: FontWeight.bold,
-                          letterSpacing: 1.5,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                TextField(
-                  controller: _usernameController,
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.person, color: Colors.white),
-                    labelText: 'Username',
-                    hintText: 'Enter username',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                      borderSide: BorderSide.none,
-                    ),
-                    filled: true,
-                    fillColor: Colors.white.withOpacity(0.3),
-                  ),
-                  style: TextStyle(color: Colors.white),
-                ),
-                SizedBox(height: 16.0),
-                TextField(
-                  controller: _emailController,
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.email, color: Colors.white),
-                    labelText: 'Email',
-                    hintText: 'Enter email',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                      borderSide: BorderSide.none,
-                    ),
-                    filled: true,
-                    fillColor: Colors.white.withOpacity(0.3),
-                  ),
-                  style: TextStyle(color: Colors.white),
-                ),
-                SizedBox(height: 16.0),
-                TextField(
-                  controller: _passwordController,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.lock, color: Colors.white),
-                    labelText: 'Create Password',
-                    hintText: 'Enter password',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                      borderSide: BorderSide.none,
-                    ),
-                    filled: true,
-                    fillColor: Colors.white.withOpacity(0.3),
-                  ),
-                  style: TextStyle(color: Colors.white),
-                ),
-                SizedBox(height: 24.0),
-                ElevatedButton(
-                  onPressed: () async => await signUp(
-                    context,
-                    _emailController.text.trim(),
-                    _passwordController.text.trim(),
-                    _usernameController.text.trim(),
-                  ),
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.white,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                  ),
-                  child: Container(
-                    padding: EdgeInsets.symmetric(vertical: 12.0),
-                    child: Center(
-                      child: Text(
-                        'Sign Up',
-                        style:
-                            TextStyle(fontSize: 16.0, color: Color(0xFF004080)),
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 12.0),
-                TextButton(
-                  onPressed: () => _goToLogin(context),
-                  child: Text(
-                    'Already have an account? Login',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-              ],
-            ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xFF0088FF), // Light Blue
+              Color(0xFF004080), // Dark Blue
+            ],
           ),
         ),
-      ),
-    );
-  }
-
-  void _goToLogin(BuildContext context) {
-    Navigator.pushNamed(context, '/login');
-  }
-}
-
-class _HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Home Page'),
-      ),
-      body: Center(
-        child: Text('Welcome to the Home Page!'),
-      ),
-    );
-  }
-}
-
-class _LoginPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Login Page'),
-      ),
-      body: Center(
-        child: Text('This is the Login Page.'),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              // Your App Logo and Name
+              Padding(
+                padding: const EdgeInsets.only(bottom: 32.0),
+                child: Column(
+                  children: [
+                    // Replace the image path with your asset image
+                    Image.asset(
+                      'assets/images/logo.png',
+                      height: 220.0,
+                      width: 150.0,
+                      color: Colors.white,
+                    ),
+                    SizedBox(height: 16.0),
+                    Text(
+                      'MoneyMinder',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 1.5,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              // Username TextField
+              TextField(
+                controller: _usernameController,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.person, color: Colors.white),
+                  labelText: 'Username',
+                  hintText: 'Enter username',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: BorderSide.none,
+                  ),
+                  filled: true,
+                  fillColor: Colors.white.withOpacity(0.3),
+                ),
+                style: TextStyle(color: Colors.white),
+              ),
+              SizedBox(height: 16.0),
+              // Email TextField
+              TextField(
+                controller: _emailController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.email, color: Colors.white),
+                  labelText: 'Email',
+                  hintText: 'Enter email',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: BorderSide.none,
+                  ),
+                  filled: true,
+                  fillColor: Colors.white.withOpacity(0.3),
+                ),
+                style: TextStyle(color: Colors.white),
+              ),
+              SizedBox(height: 16.0),
+              // Password TextField
+              TextField(
+                controller: _passwordController,
+                obscureText: true,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.lock, color: Colors.white),
+                  labelText: 'Password',
+                  hintText: 'Enter password',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                    borderSide: BorderSide.none,
+                  ),
+                  filled: true,
+                  fillColor: Colors.white.withOpacity(0.3),
+                ),
+                style: TextStyle(color: Colors.white),
+              ),
+              SizedBox(height: 24.0),
+              // Signup Button
+              ElevatedButton(
+                onPressed: () => _signUp(context),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+                child: Container(
+                  padding: EdgeInsets.symmetric(vertical: 12.0),
+                  child: Center(
+                    child: Text(
+                      'Sign Up',
+                      style:
+                          TextStyle(fontSize: 16.0, color: Color(0xFF004080)),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 16.0),
+              // Already have an account? Login
+              TextButton(
+                onPressed: () => _navigateToLogin(context),
+                child: Text(
+                  'Already have an account? Login',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
@@ -214,15 +205,9 @@ class _LoginPage extends StatelessWidget {
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-
   runApp(
     MaterialApp(
-      initialRoute: '/signup',
-      routes: {
-        '/signup': (context) => SignUpPage(),
-        '/home': (context) => HomePage(),
-        '/login': (context) => LoginPage(),
-      },
+      home: SignUpPage(),
     ),
   );
 }
